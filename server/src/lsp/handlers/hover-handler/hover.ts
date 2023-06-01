6ece4ccb7f046bc3959240c8c8dd42c5bdcea096
Spiros Grigoratos <57859686+Traigor@users.noreply.github.com>
2023-06-01 17:42:28 +0300

    Develop (#29)
    * fixed bugs of hover

--- server/src/lsp/handlers/hover-handler/hover.ts
+++ server/src/lsp/handlers/hover-handler/hover.ts
@@ -13,7 +13,7 @@ export const handleHover = (
   const document = TextDocument.create(params.textDocument.uri, 'bitloops', 1, fileContent);
   const position = params.position;
   const boundedContext = stateManager.getBoundedContext(params.textDocument.uri);
-
+  if (boundedContext === 'unknown') return null; //if the bounded context is unknown, we don't want to have hover provider
   let word = findWord(document, position);
   const symbolTable = analyzer.getSymbolTable() as TSymbolTableSemantics;
   if (!symbolTable) return null;
@@ -49,32 +49,39 @@ const findWord = (document: TextDocument, position: Position): string => {
   //for example: if the word is this.accountRepo.getById(accountId).ifError(), the array will contain the following elements:
   //[this, this.accountRepo, this.accountRepo.getById(), accountId, this.accountRepo.getById().ifError()] in this order
   for (const match of matches) {
-    if (match.includes('.')) {
-      const separateMatches = match.split('.');
-      let i = 1;
-      allMatches.push(separateMatches[0]);
-      while (i < separateMatches.length - 1) {
-        allMatches.push(allMatches[allMatches.length - 1] + '.' + separateMatches[i]);
-        i++;
+    try {
+      if (match.includes('.')) {
+        const separateMatches = match.split('.');
+        let i = 1;
+        allMatches.push(separateMatches[0]);
+        while (i < separateMatches.length - 1) {
+          allMatches.push(allMatches[allMatches.length - 1] + '.' + separateMatches[i]);
+          i++;
+        }
       }
-    }
-    if (match === 'ifError(') allMatches.push(allMatches[allMatches.length - 2] + '.ifError()');
-    //special case for ifError: we want to concatenate with the previous method called, but without the variable in the parenthesis
-    else if (match.includes('(')) allMatches.push(match + ')');
-    //for methods: we keep the opening parenthesis in the matched word so we can separate the method from the variable and then we add the closing parenthesis
-    //because in the symbolTable it is stored as a method call, for example this.accountRepo.getById()
-    else allMatches.push(match);
+      if (match === 'ifError(') allMatches.push(allMatches[allMatches.length - 2] + '.ifError()');
+      //special case for ifError: we want to concatenate with the previous method called, but without the variable in the parenthesis
+      else if (match.includes('(')) allMatches.push(match + ')');
+      //for methods: we keep the opening parenthesis in the matched word so we can separate the method from the variable and then we add the closing parenthesis
+      //because in the symbolTable it is stored as a method call, for example this.accountRepo.getById()
+      else allMatches.push(match);
+    } catch (e) {}
   }
   for (let i = 0; i < allMatches.length; i++) {
-    let myLength = allMatches[i].length;
-    if (allMatches[i].includes('.')) {
-      myLength -= allMatches[i - 1].length;
-    } //if the word contains a dot, we subtract the length of the previous word from the length of the current word, as we keep the dot member in the previous word
-    if (currentPosition + myLength >= position.character && currentPosition <= position.character) {
-      return allMatches[i];
-    } else {
-      currentPosition += myLength + 1;
-    }
+    try {
+      let myLength = allMatches[i].length;
+      if (allMatches[i].includes('.')) {
+        myLength -= allMatches[i - 1].length;
+      } //if the word contains a dot, we subtract the length of the previous word from the length of the current word, as we keep the dot member in the previous word
+      if (
+        currentPosition + myLength >= position.character &&
+        currentPosition <= position.character
+      ) {
+        return allMatches[i];
+      } else {
+        currentPosition += myLength + 1;
+      }
+    } catch (e) {}
   }
   return undefined;
 };
